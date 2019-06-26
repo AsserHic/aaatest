caa1f5fc632287e7a66fbe5f684198b07ed7f630
Marko Laakso <marko.laakso@valuemotive.com>
2019-06-26 11:10:32 +0300

    Polishing code

--- src/AAATest/HumanDetector.cpp
+++ src/AAATest/HumanDetector.cpp
@@ -14,17 +14,17 @@ HumanDetector::HumanDetector(int pin, unsigned int maxAway) {
 }
 
 int HumanDetector::check() {
-  boolean isPresent   = digitalRead(m_pin);
-  int     result      = 0;
+  boolean isPresent = digitalRead(m_pin);
+  int     result    = 0;
 
   if (isPresent) {
     if (m_lastSeen < 1) {
        result = 1;
     }
-    m_lastSeen = millis() + m_maxAway;
+    m_lastSeen = millis();
   } else {    
     if ((m_lastSeen > 0) &&
-        (m_lastSeen < millis())) {
+        (m_lastSeen < millis() - m_maxAway)) {
        m_lastSeen = 0;
        result     = -1;
     }
